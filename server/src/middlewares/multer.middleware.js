38bb544c73aff2b60ee49f296ea16deed7ff5dd3
abhishek <shukla13122000@gmail.com>
2024-12-03 12:48:11 +0530

    add user route, middleware and register user endpoint

---
+++ server/src/middlewares/multer.middleware.js
@@ -0,0 +1,14 @@
+import multer from "multer";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Set the destination folder for uploaded files
+    cb(null, "./public/imgs");
+  },
+  filename: function (req, file, cb) {
+    // Set the filename for the uploaded file
+    cb(null, file.originalname);
+  },
+});
+
+// Create the upload middleware
+export const upload = multer({ storage: storage });
