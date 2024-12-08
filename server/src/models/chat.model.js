28e70a58fe7966b9a2c63e5ca6cbc6b0574645ab
abhishek <shukla13122000@gmail.com>
2024-12-08 14:23:05 +0530

    add chat model

---
+++ server/src/models/chat.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+    ],
+    messages: [
+      {
+        sender: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+          required: true,
+        },
+        content: {
+          type: String,
+          required: true,
+        },
+        timestamp: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+    lastMessage: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Chat = mongoose.model("Chat", chatSchema);
